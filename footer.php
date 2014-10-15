c8e456df2cd3ea942c5fe391a83ef5b35c4a35fd
Alfau <alfauadam.16@gmail.com>
2014-10-15 23:33:58 +0500

    design changes

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 <footer>
 	<span>Copyright 2014 ELL MALDIVES. All Rights Reserved.</span>
 	<ul>
-		<li><a href=#><img src="icons/facebook.png" alt="fb"/></a></li>
-		<li><a href=#><img src="icons/twitter.png" alt="twi"/></a></li>
-		<li><a href=#><img src="icons/google.png" alt="goog"/></a></li>
+		<li><a href=#><img src="icons/g+.png" alt="goog"/></a></li>
+		<li><a href=#><img src="icons/yt.png" alt="goog"/></a></li>
+		<li><a href=#><img src="icons/tw.png" alt="twi"/></a></li>
+		<li><a href=#><img src="icons/fb.png" alt="fb"/></a></li>
 	</ul>
 </footer>
