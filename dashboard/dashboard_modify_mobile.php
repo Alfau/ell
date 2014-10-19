eee88d2f86a07414ad5c1bac9e8c6c372527f376
Alfau <alfauadam.16@gmail.com>
2014-10-19 19:29:11 +0500

    dashboard uploads work as intended

--- dashboard/dashboard_modify_mobile.php
+++ dashboard/dashboard_modify_mobile.php
@@ -25,6 +25,14 @@
 					
 				}else{
 					
+					?>
+						<div class="manage_options">
+							<h4>Would you like to :</h4>
+							<p><a href="dashboard_modify_mobile.php?how_manage=add"><span>&nbsp;</span>Add Mobile Products</a></p>
+							<p><a href="dashboard_modify_mobile.php?how_manage=remove"><span>&nbsp;</span>Remove Mobile Products</a></p>
+						</div>
+					<?php
+					
 					if(!isset($_GET['modify_mobile_category'])){
 					?>
 					
@@ -34,19 +42,33 @@
 							<p><a href="<?php echo $_SERVER['REQUEST_URI'] ?>&modify_mobile_category=Tablets"><span>&nbsp;</span>Tablets</a></p>
 							<p><a href="<?php echo $_SERVER['REQUEST_URI'] ?>&modify_mobile_category=Mobile Accessories"><span>&nbsp;</span>Mobile Accessories</a></p>
 						</div>
-						
-						<div id="content"></div>
 					
 				<?php	
+					//the problems as of now
+					// - div#content is not added via ajax for some reason
+					// - the second js function is wrong because it tries to add everything in main when it just needs to add the form
+					// - also theres some ugly code in there
 					}else{
+						
+						?>
+						
+						<div class="manage_category">
+							<h4>Choose a product category :</h4>
+							<p><a href="<?php echo $_SERVER['REQUEST_URI'] ?>&modify_mobile_category=Mobile Phones"><span>&nbsp;</span>Mobile Phones</a></p>
+							<p><a href="<?php echo $_SERVER['REQUEST_URI'] ?>&modify_mobile_category=Tablets"><span>&nbsp;</span>Tablets</a></p>
+							<p><a href="<?php echo $_SERVER['REQUEST_URI'] ?>&modify_mobile_category=Mobile Accessories"><span>&nbsp;</span>Mobile Accessories</a></p>
+						</div>
+						
+						<?php
 								
 						$how_manage=$_GET['how_manage'];
 						$modify_mobile_category=$_GET['modify_mobile_category'];
 						
 						if($how_manage=="add" && ($modify_mobile_category=="Mobile Phones" || $modify_mobile_category=="Tablets")){
 						?>
+							<div id="content">
 							<h4>Add <?php echo $modify_mobile_category; ?> to Database</h4>
-							<form method="POST" action="" enctype="multipart/form-data">
+							<form method="POST" action="dashboard_modify_mobile.php?how_manage=add&modify_mobile_category=Mobile Phones" enctype="multipart/form-data">
 								<?php include("dashboard_modify_handler.php");?>
 								<table>
 									<tr>
@@ -95,6 +117,7 @@
 								<input type="hidden" name="add_what" value="<?php echo $modify_mobile_category ?>"/>
 								<input type="submit" value="Submit"/>
 							</form>
+							</div>	
 							<!--<div>
 								<h3>Useful Tips</h3>
 								<ul>
@@ -105,7 +128,58 @@
 								</ul>
 							</div>-->
 						<?php	
-						}
+						}elseif($modify_mobile_category=="Mobile Accessories"){
+				?>		<div id="content">
+						<form method="POST" action="" enctype="multipart/form-data">
+							<h4>Add Mobile Accessories from Website</h4>
+							<?php include("dashboard_modify_handler.php");?>
+							<table>
+								<tr>
+									<td>Choose Brand :</td>
+									<td>
+										<select name="brand">
+											<option value="Samsung">Samsung</option>
+											<option value="Apple">Apple</option>
+											<option value="HTC">HTC</option>
+											<option value="LG">LG</option>
+											<option value="Other">Other</option>
+										</select>
+									</td>
+								</tr>
+								<tr>
+									<td>Type :</td>
+									<td>
+										<select name="category">
+											<option value="Battery">Battery</option>
+											<option value="Charger">Charger</option>
+											<option value="Headset">Headset</option>
+											<option value="Screen Protection">Screen Protection</option>
+											<option value="Protective Cases">Protective Cases</option>
+											<option value="Other">Other</option>
+										</select>
+									</td>
+								</tr>
+								<tr>
+									<td>Name :</td>
+									<td><input type="text" name="name"/></td>
+								</tr>
+								<tr>
+									<td>Price(Rf) :</td>
+									<td><input type="text" name="price"/></td>
+								</tr>
+								<tr>
+									<td>Image Thumbnail :</td>
+									<td>
+										<input type="file" name="image_thumbnail"/>
+									</td>
+								</tr>
+							</table>
+							<input type="hidden" name="add_what" value="Mobile Accessories"/>
+							<input type="submit" value="Submit"/>
+						</form>
+					</div>	
+				<?php		
+					}
 							
 					}
 							
--- dashboard/dashboard_script.js
+++ dashboard/dashboard_script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-	var height=$("html").height();
 	
 	$(document).on("click","div.manage_options a",function(e){
 		$("div.manage_options a").removeClass("active");
 		$(this).addClass("active");
 		var url=$(this).prop("href");
-		var how_manage=url.split("=")[1]; //global variable is ugly
+		var how_manage=url.split("=")[1];
 		
 		$.get("dashboard_modify_mobile.php",{how_manage:how_manage},function(data){
-			var content=$(data).find("div.manage_category").parent().html();
-			$("div.manage_options").after(content);
+			var content=$(data).find("div.manage_category").html();
+			$("div.manage_options").after("<div class='manage_category'>"+content+"</div>"); // ugly code so change asap
+			//$("div.manage_category").after("<div id='content'></div>");
 		});
 		e.preventDefault();
 	});
@@ -22,8 +22,8 @@ $(document).ready(function(){
 		var modify_mobile_category=(url.split("=")[2]).replace("%20"," ");
 		
 		$.get("dashboard_modify_mobile.php",{how_manage:how_manage,modify_mobile_category:modify_mobile_category},function(data){
-			var content=$(data).filter("main").html();
-			$("div#content").html(content);
+			var content=$(data).find("div#content").html();
+			$("div.manage_category").after("<div id='content'>"+content+"</div>");
 		});
 		e.preventDefault();
 	});
