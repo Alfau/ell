4915f43efc0d5f03205115e1803532cf5698b3e5
Alfau <alfauadam.16@gmail.com>
2014-10-26 06:24:12 +0500

    about images can now be removed via dashboard

--- dashboard/dashboard_modify_about.php
+++ dashboard/dashboard_modify_about.php
@@ -11,31 +11,11 @@
 		<?php include("dashboard_nav.php"); ?>
 		
 		<main>
-			<div id="modify_about">
-				<h4>Modify About Us page</h4>
+			<div>
+				<h4>Modify Text in About Us Page</h4>
 				<?php
 				include("../connection.php");
 				if(isset($_POST['about'])){
-					foreach($_FILES['about_img']['name'] as $index => $value){
-						if($_FILES['about_img']['name'][$index]!=""){
-							$temp_filename=$_FILES['about_img']['tmp_name'][$index];
-							$original_filename=$_FILES['about_img']['name'][$index];
-							$new_filename=md5($original_filename).mt_rand().".jpg";//change to allow more image types
-							$destination="../page_images/".$new_filename;
-							$image_path="page_images/".$new_filename;
-							$image_id=$index+1;
-							
-							if(move_uploaded_file($temp_filename, $destination)){
-								$result=mysqli_query($con,"SELECT Images FROM about_img WHERE ID='$image_id'");
-								$old_image=mysqli_fetch_array($result);
-								unlink("../".trim($old_image['Images']));
-								
-								if(!mysqli_query($con,"UPDATE about_img SET Images='$image_path' WHERE ID='$image_id'")){ // merge both queries
-									echo "<p class='failed'>Database Update Failed. Try Again.</p>";// change to a better message later
-								}
-							}
-						}
-					}
 					$about=$_POST['about'];
 					$query="UPDATE about SET Text='$about'";
 					
@@ -45,33 +25,14 @@
 						echo "<p class='success'>Database successfuly updated!</p>";
 					}
 				}
-				
-					$about_query=mysqli_query($con,"SELECT * FROM about");
+					$about_query=mysqli_query($con,"SELECT Text FROM about");
 					while($row=mysqli_fetch_array($about_query)){
 						?>
-						<form action="" method="POST" enctype="multipart/form-data">
+						<form action="" method="POST">
 							<table>
 								<tr>
 									<td>Text :</td>
 									<td><textarea name="about" rows="10" cols="100"><?php echo $row['Text'] ?></textarea></td>
-								</tr>
-								<tr>
-									<td>Images* :</td>
-									<td>
-									<?php
-									$query="SELECT * FROM about_img";
-									$result=mysqli_query($con,$query);
-									while($row=mysqli_fetch_array($result)){
-										?>
-										<label>
-											<img src='../<?php echo $row['Images'] ?>' height='100'/>
-											<input type='file' name='about_img[]'/>
-										</label>
-										<?php	
-									}
-									?>
-									<p style='font-size:0.8em;color:#888'>* Upto 5 images allowed!</p>
-									</td>
 								</tr>
 							</table>
 							<input type="submit" value="Update"/>
@@ -80,8 +41,80 @@
 					}
 				?>
 			</div>
+			<div id="modify_about">
+				<h4>Manage Images in About Us Page</h4>
+				<?php
+				
+				if(isset($_POST['replace_img'])){
+					
+					if(isset($_POST['remove_img'])){
+						$remove_id=$_POST['remove_img'];
+						$query="DELETE FROM about_img WHERE ID='$remove_id'";
+					}
+					foreach($_FILES['about_img']['name'] as $index => $value){
+						if($_FILES['about_img']['name'][$index]!=""){
+							$temp_filename=$_FILES['about_img']['tmp_name'][$index];
+							$original_filename=$_FILES['about_img']['name'][$index];
+							$new_filename=md5($original_filename).mt_rand().".jpg";//change to allow more image types
+							$destination="../page_images/".$new_filename;
+							$image_path="page_images/".$new_filename;
+							$image_id=$index+1;
+							
+							
+							if(move_uploaded_file($temp_filename, $destination)){
+								$result=mysqli_query($con,"SELECT Images FROM about_img WHERE ID='$image_id'");
+								$old_image=mysqli_fetch_array($result);
+								unlink("../".trim($old_image['Images']));
+								
+								$query="UPDATE about_img SET Images='$image_path' WHERE ID='$image_id'";
+							}
+						}
+					}
+					if(!mysqli_query($con,$query)){
+						echo "<p class='failed'>Database Update Failed. Try Again.</p>";// change to a better message later and also include a success message
+					}
+				}
+				?>
+				<form method="POST" action="" enctype="multipart/form-data">
+					<table>
+						<tr>
+							<td>Images :</td>
+							<?php
+							$query="SELECT * FROM about_img";
+							$result=mysqli_query($con,$query);
+							while($row=mysqli_fetch_array($result)){
+								?>
+								<td>
+									<label>
+										<img src='../<?php echo $row['Images'] ?>' height='100'/>
+										<input type='file' name='about_img[]'/>
+										<input type='hidden' name='replace_img'/>
+									</label>
+								</td>
+								<?php	
+							}
+							?>
+						</tr>
+						<tr>
+							<td>Remove Image:</td>
+							<?php
+							$query="SELECT ID FROM about_img";
+							$result=mysqli_query($con,$query);
+							while($row=mysqli_fetch_array($result)){
+								?>
+								<td>
+									<input type='checkbox' name='remove_img' value="<?php echo $row['ID'] ?>"/>
+								</td>
+								<?php
+							}
+							?>
+						</tr>
+					</table>
+					<input type="submit" value="Update"/>
+				</form>
+			</div>
 			<div>
-				<h4>Add Image to Page</h4>
+				<h4>Add Image to About Us Page</h4>
 				<?php
 				if(isset($_POST['add_img'])){
 					if($_FILES['about_img']['size']!==0){
@@ -112,13 +145,14 @@
 				<form method="POST" action="" enctype="multipart/form-data">
 					<table>
 						<tr>
+							<td>Add Image :</td>
 							<td>
 								<input type="file" name="about_img"/>
 								<input type="hidden" name="add_img"/>
 							</td>
 						</tr>
 					</table>
-					<input type="submit"/>
+					<input type="submit" value="Submit"/>
 				</form>
 			</div>
 		</main>
