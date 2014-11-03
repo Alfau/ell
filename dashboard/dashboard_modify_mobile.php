c14774ef5e9ecdcd25584206c6f8a8dec064e947
Alfau <alfauadam.16@gmail.com>
2014-11-03 12:00:36 +0500

    ell rec to multimedia and home appliance

--- dashboard/dashboard_modify_mobile.php
+++ dashboard/dashboard_modify_mobile.php
@@ -18,6 +18,7 @@
 						<h4>Choose an option :</h4>
 						<p><a href="dashboard_modify_mobile.php?how_manage=add" class="add"><span>&nbsp;</span>Add Mobile Products</a></p>
 						<p><a href="dashboard_modify_mobile.php?how_manage=remove" class="remove"><span>&nbsp;</span>Remove Mobile Products</a></p>
+						<p><a href="dashboard_modify_mobile.php?how_manage=ell_rec" class="ell_rec"><span>&nbsp;</span>Choose Ell Recommended Products</a></p>
 					</div>
 					
 				<?php
@@ -29,6 +30,7 @@
 							<h4>Choose an option :</h4>
 							<p><a href="dashboard_modify_mobile.php?how_manage=add" class="add"><span>&nbsp;</span>Add Mobile Products</a></p>
 							<p><a href="dashboard_modify_mobile.php?how_manage=remove" class="remove"><span>&nbsp;</span>Remove Mobile Products</a></p>
+							<p><a href="dashboard_modify_mobile.php?how_manage=ell_rec" class="ell_rec"><span>&nbsp;</span>Choose Ell Recommended Products</a></p>
 						</div>
 						
 					<?php
@@ -235,6 +237,51 @@
 								</div>
 								
 							<?php
+						}elseif($how_manage=="ell_rec"){
+							?>	
+							<div class="content">
+								<h4>Choose product to recommend!</h4>
+								<br /><p class="asterix"> * A list of the recent products uploaded to the website. If the product you want to choose is not here, use the search function below to find it.</p>	
+									<table>
+									<?php 
+									include("dashboard_manage_products_handler.php");
+									include("../connection.php");
+									$query="SELECT * FROM products WHERE Type='$modify_category' AND Ell_Rec='False' ORDER BY ID DESC LIMIT 5";
+									$result=mysqli_query($con,$query);
+									while($row=mysqli_fetch_array($result)){
+										?>
+										<tr>
+											<td><img src="<?php echo "../".$row['Thumbnail'] ?>" height="120"/></td>
+											<td>
+												<form method="POST" action="dashboard_modify_multimedia?how_manage=ell_rec&modify_category=<?php echo $modify_category ?>">
+													<span class="product_name_small"><?php echo $row['Name'] ?></span><br />
+													<span class="light">Price : </span><span class="price">Rf. <?php echo $row['Price'] ?></span><br /><?php echo $row['ID']; ?>
+													<br /><input type="submit" name="ell_rec" value="Recommend"/>
+													<input type="hidden" name="product_ID" value="<?php echo $row['ID'] ?>"/>
+												</form>
+											</td>
+										</tr>
+									<?php
+									}
+									?>
+									</table>
+							</div>
+							
+							<div class="content">
+								<h4>Search for the product you want to recommend</h4>
+								<form method="POST" action="dashboard_modify_mobile?how_manage=remove&modify_category=<?php echo $modify_category ?>">
+									<table>
+										<tr>
+											<td>Search : </td>
+											<td><input type="text" name="search_and_recommend"/></td>
+										</tr>
+										<tr>
+											<td><input type="submit" value="Search"/></td>
+										</tr>
+									</table>
+								</form>
+							</div>
+						<?php
 						}
 					}	
 				}
